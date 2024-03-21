d0843ea84e1ee553c16629f3745c92ba470044b7
enrico-amerio <amerio.enrico95@gmail.com>
2024-03-21 16:44:50 +0100

    [add] Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,67 @@
 <script>
   export default {
+    data(){
+      return{
+        categories: [
+          {name: 'Donna', link:'#'},
+          {name: 'Uomo', link:'#'},
+          {name: 'Bambino', link:'#'}
+        ],
+        icons: [
+          {iconName: 'user', class:'fa-regular fa-user', link:'#'},
+          {iconName: 'heart', class:'fa-regular fa-heart', link:'#'},
+          {iconName: 'shop', class:'fa-solid fa-bag-shopping', link:'#'}
+        ]
+
+      }
+    }
         
   }
 </script>
 <template>
+  <header>
   <div>
-Header
+    <a v-for="(category, index) in categories" :key="index" :href="category.link" class="top-link">{{ category.name }}</a>
   </div>
+  <div >
+    <img src="/img/boolean-logo.png" alt="boolean-logo" id="logo">
+  </div>
+  <div class="icon">
+    <a v-for="(icon, index) in icons" :key="index" :href="icon.link">
+      <i :class="icon.class"></i>
+    </a> 
+  </div>
+ </header>
 </template>
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
 
+header{
+  height:50px;
+  background-color: $orange;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color:white;
+  font-weight: bold;
+  margin-bottom: 20px;
+  box-shadow: 0 1px 3px grey;
+  width: 100%;
+  position: fixed;
+  z-index: 999;
+  a{
+    text-decoration: none;
+    color: white;
+    padding: 5px;
+    &:hover{
+      text-decoration: underline;
+    }
+  }
+}
+#logo{
+  width: 100px;
+  height:24px;
+}
 </style>
